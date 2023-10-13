c550530e4dbdfa7d8f5505aad062afb8d5ad56fe
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-10-14 00:52:26 +0200

    Build also for `aarch64-linux`

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux"];
       imports = [
         ./packages
         ./hmModules
