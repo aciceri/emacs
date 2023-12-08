041e989785b0869a8d8a88fc7d2cc890f7184307
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-08 10:41:14 +0100

    Hercules CI `flake-update` effect

--- ci/default.nix
+++ ci/default.nix
@@ -6,4 +6,15 @@
     "x86_64-linux"
     # "aarch64-linux"
   ];
+  hercules-ci.flake-update = {
+    enable = true;
+    updateBranch = "updated-flake-lock";
+    createPullRequest = true;
+    autoMergeMethod = null;
+    when = {
+      minute = 30;
+      hour = 13;
+      dayOfWeek = ["Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"];
+    };
+  };
 }
